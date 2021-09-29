e7e4e98b8a512649a3c0b764fc97d3dc242f8745
unknown <ahmedheakl2020@gmail.com>
2021-09-29 04:49:44 +0200

    authors page created

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorsRouter = require("./routes/authors");
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
@@ -21,6 +23,10 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
+// Routes
 app.use("/", indexRouter);
+app.use("/authors", authorsRouter);
 
 app.listen(process.env.PORT || 3000);
