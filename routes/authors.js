57eeefd63e7ce9a93c5a5e657e3aa19e2c7c6701
unknown <ahmedheakl2020@gmail.com>
2021-09-30 03:58:44 +0200

    books and authors pages added

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
+const Book = require("../models/book");
 
 router.get("/", async (req, res) => {
   let searchOptions = {};
@@ -25,12 +26,63 @@ router.post("/", async (req, res) => {
   });
   try {
     const newAuth = await author.save();
-    res.redirect("authors");
+    res.redirect(`authors/${newAuth.id}`);
+    // res.redirect("authors");
   } catch {
     res.render("authors/new", {
       author: author,
       errMessage: "Error Creating an Author",
     });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: author.id }).limit(6).exec();
+    res.render("authors/view", { author: author, books: books });
+  } catch {
+    res.redirect("/");
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  const author = await Author.findById(req.params.id);
+  res.render("authors/edit", { author: author });
+});
+
+router.put("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    console.log("DONE");
+    res.redirect(`/authors/${author.id}`);
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.render(`authors/edit`, {
+        author: author,
+        errMessage: "Error Updating an Author",
+      });
+    }
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    await author.remove();
+    res.redirect("/authors");
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.redirect(`/authors/${author.id}`);
+    }
   }
 });
 
--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorsRouter = require("./routes/authors");
+const booksRouter = require("./routes/books");
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,9 +26,11 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(methodOverride("_method"));
 
 // Routes
 app.use("/", indexRouter);
 app.use("/authors", authorsRouter);
+app.use("/books", booksRouter);
 
 app.listen(process.env.PORT || 3000);
